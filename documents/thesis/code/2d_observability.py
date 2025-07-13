132c52fe020e93d97745c393af4e90e8304ebb37
Tom Zurales <tomzurales@gmail.com>
2025-07-13 16:28:39 -0700

    Automated commit: changes in documents/ directory at 2025-07-13 16:28:39

--- documents/thesis/code/2d_observability.py
+++ documents/thesis/code/2d_observability.py
@@ -43,7 +43,7 @@ plt.colorbar(contour, label='Combined Gaussian Value')
 
 # Draw black rectangle from (0,0) to (3,3)
 from matplotlib.patches import Rectangle
-rect = Rectangle((0, 0), 3, 3, linewidth=2, edgecolor='black', facecolor='none')
+rect = Rectangle((0, 0), 3, 3, linewidth=2, edgecolor='black', facecolor='black')
 plt.gca().add_patch(rect)
 
 plt.xlabel('X')
