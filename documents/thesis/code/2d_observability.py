7f4bfecf61cba3f9cd7291d2f95f38c79ba4c544
Tom Zurales <tomzurales@gmail.com>
2025-07-22 20:45:03 -0700

    Automated commit: changes in documents/ directory at 2025-07-22 20:45:03

--- documents/thesis/code/2d_observability.py
+++ documents/thesis/code/2d_observability.py
@@ -9,14 +9,6 @@ fig, ax = plt.subplots(subplot_kw=dict(projection='polar'))
 ax.plot(0, 0, 'ro', markersize=8, label='p')
 ax.text(0, 0.1, 'p', ha='center', va='bottom', fontsize=12)
 
-# Draw black line from 7π/4 to π/4 at distance 0.4
-theta_start = 7 * np.pi / 4
-theta_end = np.pi / 4
-# Create arc from 7π/4 to π/4 (going clockwise through 0)
-theta_arc = np.linspace(theta_start, theta_end + 2*np.pi, 100)
-r_arc = np.full_like(theta_arc, 0.4)
-ax.plot(theta_arc, r_arc, 'k-', linewidth=2)
-
 # Set up the plot
 ax.set_ylim(0, 1)
 ax.set_yticklabels([])  # Remove radial distance labels
