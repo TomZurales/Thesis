392cffe914d7dcf5baa5ac955a49e473eca60c6a
Tom Zurales <tomzurales@gmail.com>
2025-07-22 21:21:54 -0700

    Automated commit: changes in documents/ directory at 2025-07-22 21:21:54

--- documents/thesis/code/2d_observability.py
+++ documents/thesis/code/2d_observability.py
@@ -7,7 +7,7 @@ fig, ax = plt.subplots(subplot_kw=dict(projection='polar'))
 
 # Plot point at center
 ax.plot(0, 0, 'ro', markersize=8, label='p')
-ax.text(0, 0.1, 'p', ha='center', va='bottom', fontsize=12)
+ax.text(0, 0.1, '$p$', ha='center', va='bottom', fontsize=12)
 
 # Set up the plot
 ax.set_ylim(0, 1)
