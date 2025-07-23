0af23ff2018d2cc0007cc43969770af22677a214
Tom Zurales <tomzurales@gmail.com>
2025-07-22 20:28:32 -0700

    Automated commit: changes in documents/ directory at 2025-07-22 20:28:32

--- documents/thesis/code/2d_observability.py
+++ documents/thesis/code/2d_observability.py
@@ -12,8 +12,10 @@ ax.text(0, 0.1, 'p', ha='center', va='bottom', fontsize=12)
 # Set up the plot
 ax.set_ylim(0, 1)
 ax.set_yticklabels([])  # Remove radial distance labels
+# Create radian labels as fractions of pi
+radian_labels = ['0', 'π/4', 'π/2', '3π/4', 'π', '5π/4', '3π/2', '7π/4']
 ax.set_thetagrids(np.degrees(np.linspace(0, 2*np.pi, 8, endpoint=False)), 
-                  labels=[f'{x:.2f}' for x in np.linspace(0, 2*np.pi, 8, endpoint=False)])
+                  labels=radian_labels)
 ax.set_title('Radial Plot')
 ax.grid(True)
 
