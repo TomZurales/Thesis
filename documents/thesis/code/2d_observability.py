6d0054be5fd39d1cb064219bf64b43bba80b9831
Tom Zurales <tomzurales@gmail.com>
2025-07-22 21:29:55 -0700

    Automated commit: changes in documents/ directory at 2025-07-22 21:29:55

--- documents/thesis/code/2d_observability.py
+++ documents/thesis/code/2d_observability.py
@@ -9,6 +9,12 @@ fig, ax = plt.subplots(subplot_kw=dict(projection='polar'))
 ax.plot(0, 0, 'ro', markersize=8, label='p')
 ax.text(0, 0.1, '$p$', ha='center', va='bottom', fontsize=12)
 
+# Color the area from 7π/4 to π/4 where r < 0.4 blue
+theta_range = np.linspace(7*np.pi/4, np.pi/4 + 2*np.pi, 100)  # Add 2π to handle wrap-around
+r_inner = np.zeros_like(theta_range)
+r_outer = np.full_like(theta_range, 0.4)
+ax.fill_between(theta_range, r_inner, r_outer, alpha=0.3, color='blue')
+
 # Set up the plot
 ax.set_ylim(0, 1)
 ax.set_yticklabels([])  # Remove radial distance labels
