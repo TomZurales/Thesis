fcbc61e10d3904a95bbbabbc46429217389884f7
Tom Zurales <tomzurales@gmail.com>
2025-07-22 21:24:14 -0700

    Automated commit: changes in documents/ directory at 2025-07-22 21:24:14

--- documents/thesis/code/2d_observability.py
+++ documents/thesis/code/2d_observability.py
@@ -9,7 +9,7 @@ fig, ax = plt.subplots(subplot_kw=dict(projection='polar'))
 ax.plot(0, 0, 'ro', markersize=8, label='p')
 ax.text(0, 0.1, '$p$', ha='center', va='bottom', fontsize=12)
 
-theta = np.arange(0, 2)
+theta = np.arange(0, 2 * np.pi, 0.01)
 
 # Set up the plot
 ax.set_ylim(0, 1)
