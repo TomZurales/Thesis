fa91dcda4a144054080068bdbe415fbfb1593755
Tom Zurales <tomzurales@gmail.com>
2025-07-23 20:14:39 -0700

    Automated commit: changes in documents/ directory at 2025-07-23 20:14:39

--- documents/thesis/code/2d_observability.py
+++ documents/thesis/code/2d_observability.py
@@ -199,7 +199,7 @@ ax2.text(0, 0.1, "$p$", ha="center", va="bottom", fontsize=12)
 obs.draw_barriers(ax2)
 obs.draw_samples(ax2, samples)
 # obs.draw_knn_model(ax2, 1, samples)
-ax2.set_title("KNN Model (k=5)")
+ax2.set_title("Sampled Observability")
 
 # # Right plot: Binned model
 # ax3.plot(0, 0, "ro", markersize=8, label="p")
