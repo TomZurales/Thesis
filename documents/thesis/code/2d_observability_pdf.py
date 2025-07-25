575af1a5d4d59b881778722ee75b002928d9f1ad
Tom Zurales <tomzurales@gmail.com>
2025-07-24 21:32:21 -0700

    Automated commit: changes in documents/ directory at 2025-07-24 21:32:21

--- documents/thesis/code/2d_observability_pdf.py
+++ documents/thesis/code/2d_observability_pdf.py
@@ -185,7 +185,8 @@ class MPO:
         else:
             temp_samples = self.samples.copy()
             # Recalculate uniqueness and confidence for every point
-            # Uniqueness = distance to nearest 
+            # Uniqueness = distance to nearest similar observation, either seen or not seen
+            for 
     
 
 
