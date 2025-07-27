a48e0bbdf549398ceb82d1d3aee03bc95812910e
Tom Zurales <tomzurales@gmail.com>
2025-07-26 17:23:06 -0700

    Automated commit: changes in documents/ directory at 2025-07-26 17:23:06

--- documents/thesis/code/2d_observability.py
+++ documents/thesis/code/2d_observability.py
@@ -242,7 +242,8 @@ ax2.legend(handles=legend_elements, loc='upper right', bbox_to_anchor=(1.05, 1.0
 # Set up all plots
 for ax in [ax1, ax2]:
     ax.set_ylim(0, 1.05)
-    print(ax.get_yticklabels())
+    y_labels = ax.get_yticklabels()
+    for la
     ax.set_yticklabels([])  # Remove radial distance labels
     # Create radian labels as fractions of pi
     radian_labels = ["0", "π/4", "π/2", "3π/4", "π", "5π/4", "3π/2", "7π/4"]
