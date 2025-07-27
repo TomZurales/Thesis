691017b4714e40f83879cb7344c66f33fac516bb
Tom Zurales <tomzurales@gmail.com>
2025-07-26 17:25:46 -0700

    Automated commit: changes in documents/ directory at 2025-07-26 17:25:46

--- documents/thesis/code/2d_observability.py
+++ documents/thesis/code/2d_observability.py
@@ -244,7 +244,7 @@ for ax in [ax1, ax2]:
     ax.set_ylim(0, 1.05)
     y_labels = ax.get_yticklabels()
     for i in range(len(y_labels)):
-        y_labels[]
+        y_labels[-1] = 
     ax.set_yticklabels(y_labels)  # Remove radial distance labels
     # Create radian labels as fractions of pi
     radian_labels = ["0", "π/4", "π/2", "3π/4", "π", "5π/4", "3π/2", "7π/4"]
